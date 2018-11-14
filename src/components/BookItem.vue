e107121947c4e696aca0d1761c2c29e8e6813144
Howard Wang <hao.1.wang@gmail.com>
2018-11-14 13:20:54 -0500

    Completed the module4

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
     name: "BookItem",
-    props: ["book"] 
+    props: ["book"],
 }
 </script>
 
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,14 +2,19 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books">{{ book.title }}:{{book.author}}</li>
+      <book-item v-for="book in books" :book="book"></book-item>
     </ul>
   </div>
 </template>
 
 <script>
+import BookItem from "./BookItem";
+
 export default {
   name: 'BookList',
+  components: {
+    BookItem
+  },
   data() {
     return {
       title: 'All Books',
@@ -32,10 +37,5 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: block;
-  margin: 0 10px;
 }
 </style>
