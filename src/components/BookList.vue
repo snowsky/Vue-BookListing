eae11f04984a954312a04ad089b80c950eff2156
Howard Wang <hao.1.wang@gmail.com>
2018-11-13 21:16:00 -0500

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
 </template>
 
 <script>
@@ -16,6 +18,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  
+  font-weight: normal
 }
 </style>
