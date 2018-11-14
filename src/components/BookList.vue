5e8db13e51ace2a02b197b208453b12ad06f2068
Howard Wang <hao.1.wang@gmail.com>
2018-11-13 21:14:06 -0500

    Completed the module

---
+++ src/components/BookList.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      title: 'All Books'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  
+}
+</style>
